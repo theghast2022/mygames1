11ebb5f48ab85e562b66f1d696a6ab414f077b69
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2018-11-19 17:03:50 +0100

    Update test.js

--- test.js
+++ test.js
@@ -59,7 +59,7 @@ client.on('ready', () => {
 });
 client.on('ready', function(){
     var ms = 100000 ;
-    var setGame = [`-help `];
+    var setGame = [`-help | Games XPRO `];
     var i = -1;
     var j = 0;
     setInterval(function (){
